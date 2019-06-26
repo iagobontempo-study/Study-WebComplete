87903a5ba0e4cc55813f2b7645248876fde66a0c
Iago Bontempo <iagobontempo@hotmail.com>
2019-06-26 15:17:54 -0300

    FlappyBird, animação da barreia

--- dom/js/flappy.js
+++ dom/js/flappy.js
@@ -47,5 +47,42 @@ function ParDeBarreiras(altura, abertura, x) {
     this.setX(x) // Posição que a barreira vai aparecer
 }
 
-const b = new ParDeBarreiras(700, 200, 400) //b = objeto criado a partir da funcao contrutora
-document.querySelector('[wm-flappy]').appendChild(b.elemento) // b.elemento é nescessario para colocar o objeto dentro da DOM
+// Apenas para testar o ParDeBarreia sendo contruida
+// const b = new ParDeBarreiras(700, 200, 400) //b = objeto criado a partir da funcao contrutora
+// document.querySelector('[wm-flappy]').appendChild(b.elemento) // b.elemento é nescessario para colocar o objeto dentro da DOM
+
+function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
+    this.pares = [ //Cria o inicio das barreiras
+        new ParDeBarreiras(altura, abertura, largura),
+        new ParDeBarreiras(altura, abertura, largura + espaco),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 2),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 3)
+    ]
+
+    const deslocamento = 3
+    this.animar = () => {
+        this.pares.forEach(par => {
+            par.setX(par.getX() - deslocamento)
+
+            //quando elemento sair da area do jogo
+            if (par.getX() < -par.getLargura()) {
+                par.setX(par.getX() + espaco * this.pares.length)
+                par.sortearAbertura()
+            }
+
+            const meio = largura / 2
+            const cruzouOMeio = par.getX() + deslocamento >= meio 
+                && par.getX() < meio
+            if (cruzouOMeio) {
+                notificarPonto()
+            }
+        })
+    }
+}
+
+const barreiras = new Barreiras(700, 1200, 200, 400)
+const areaDoJogo = document.querySelector('[wm-flappy]')
+barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+setInterval(() => {
+    barreiras.animar()
+}, 20)
