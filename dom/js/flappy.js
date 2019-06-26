e976c937bcbcc6923b69ab82c7c05b360dcc32dd
Iago Bontempo <iagobontempo@hotmail.com>
2019-06-26 16:46:51 -0300

    FlappyBird, colisões e finalização

--- dom/js/flappy.js
+++ dom/js/flappy.js
@@ -119,6 +119,33 @@ function Passaro(alturaJogo) {
 //     passaro.animar()
 // }, 20)
 
+
+//C O L I S Ã O
+function estaoSobrepostos (elementoA, elementoB) {
+    const a = elementoA.getBoundingClientRect() //Pega retangulo relacionado ao elemento A
+    const b = elementoB.getBoundingClientRect() //Pega retangulo relacionado ao elemento B
+
+    const horizontal = a.left + a.width >= b.left
+        && b.left + b.width >= a.left
+    const vertical = a.top + a.height >= b.top
+        && b.top + b.height >= a.top 
+    return horizontal && vertical
+}
+
+function colidiu(passaro, barreiras) {
+    let colidiu = false
+    barreiras.pares.forEach(parDeBarreiras => {
+        if (!colidiu) {
+            const superior = parDeBarreiras.superior.elemento
+            const inferior = parDeBarreiras.inferior.elemento
+            colidiu = estaoSobrepostos(passaro.elemento, superior) 
+                || estaoSobrepostos(passaro.elemento, inferior) 
+        }
+    })
+    return colidiu
+}
+
+
 function Progresso() {
     this.elemento = novoElemento('span', 'progresso')
     this.atualizarPontos = pontos => {
@@ -148,6 +175,10 @@ function FlappyBird() {
         const temporizador = setInterval(()=> {
             barreiras.animar()
             passaro.animar()
+
+            if (colidiu(passaro, barreiras)) {
+                clearInterval(temporizador)
+            }
         }, 20)
     }
 }
