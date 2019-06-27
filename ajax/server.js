647038e5583e93ee2aa185b6e1e5f35de7d4083c
Iago Bontempo <iagobontempo@hotmail.com>
2019-06-27 11:01:15 -0300

    Ajax, implementando upload

--- ajax/server.js
+++ ajax/server.js
@@ -8,4 +8,27 @@ app.use(express.static('.')) // esta servindo conteudo statico no caso a pasta "
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+const multer = require('multer') // Multer é utilizado para enviar aquivos
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Ocorreu um erro.')
+        }
+
+        res.end('Concluído com sucesso.')
+    })
+})
+
 app.listen(8080, () => console.log('Executando...'))
