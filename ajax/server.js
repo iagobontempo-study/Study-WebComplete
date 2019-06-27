64dd63222a949c7aeaa81809e734909d2ac51dc7
Iago Bontempo <iagobontempo@hotmail.com>
2019-06-27 14:44:19 -0300

    Axios, post e get testes

--- ajax/server.js
+++ ajax/server.js
@@ -38,4 +38,15 @@ app.post('/formulario', (req, res) => {
     })
 })
 
+app.get('/parOuImpar', (req, res) => {
+    // Dentro de express tem certas formas de receber dados do frontend
+    // 1 - req.body
+    // 2 - req.query - usaria app.get('/parOuImpar'...) - na url ficaria localhost:8080/parOuImpar?numero=1 
+    // 3 - req.params - usaria app.get('/parOuImpar/:numero'...) - na url ficaria localhost:8080/parOuImpar/1 
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
+
 app.listen(8080, () => console.log('Executando...'))
