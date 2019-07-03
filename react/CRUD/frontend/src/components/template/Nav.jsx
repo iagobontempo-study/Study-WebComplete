a4f71cbcbfd002feb7fb06827b29906a6fc2db77
Iago Bontempo <iagobontempo@hotmail.com>
2019-07-03 15:36:54 -0300

    React-Projeto Cadastro de Usuario - CRUD - componente Cadastro de Usuario (userCrud) 1

--- react/CRUD/frontend/src/components/template/Nav.jsx
+++ react/CRUD/frontend/src/components/template/Nav.jsx
@@ -5,7 +5,7 @@ export default props =>
     <aside className="menu-area">
         <nav className="menu">
             <a href="#/">
-                <i className="fa fa-home"></i>Início
+                <i className="fa fa-home"></i> Início
             </a>
             <a href="#/users">
                 <i className="fa fa-users"></i> Usuarios
