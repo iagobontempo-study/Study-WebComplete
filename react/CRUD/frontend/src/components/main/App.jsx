8ccb31088f4c4dce5d153aab9b0c5c9dd1ecbe45
Iago Bontempo <iagobontempo@hotmail.com>
2019-07-03 15:56:08 -0300

    React-Projeto Cadastro de Usuario - CRUD - Ajustando Rotas, varios imports importantes!

--- react/CRUD/frontend/src/components/main/App.jsx
+++ react/CRUD/frontend/src/components/main/App.jsx
@@ -2,16 +2,20 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+import Routes from './Routes'
 
 import Logo from '../template/Logo'
 import Nav from '../template/Nav'
-import Home from '../home/Home'
 import Footer from '../template/Footer'
 
 export default props =>
+<BrowserRouter>
     <div className="app">
         <Logo />
         <Nav />
-        <Home />
+        <Routes />
         <Footer />
     </div>
+</BrowserRouter>
--- react/CRUD/frontend/src/components/user/UserCrud.jsx
+++ react/CRUD/frontend/src/components/user/UserCrud.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Main from '../../template/Main';
+import Main from '../template/Main';
 
 const headerProps = {
     icon: 'users',
