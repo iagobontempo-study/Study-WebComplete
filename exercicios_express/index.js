cf8237f6c50c292cf8ae2cc391999ab55d831218
Iago Bontempo <iagobontempo@hotmail.com>
2019-07-05 14:42:49 -0300

    Express, tipos de resposta

--- exercicios_express/index.js
+++ exercicios_express/index.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
 
-app.use("/opa", (req, res) => {
-    res.send('<button>Estou bem!</button>')
+app.get("/opa", (req, res) => {
+   res.json([
+       {id: 1, name: 'ana', position: 1},
+       {id: 3, name: 'bia', position: 5},
+       {id: 7, name: 'carlos', position: 3}
+    ])
+   
+    // Retornar json
+    // res.json({
+    //     name: "ípad 32gb",
+    //     price: 1599.99,
+    //     discount: 0.12
+    // })
+
+    // Retornar html
+    // res.send('<button>Estou bem!</button>')
 })
 
 app.listen(3000, () => {
